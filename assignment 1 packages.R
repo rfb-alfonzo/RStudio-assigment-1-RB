e534ab1cf5aa3a4f5d82b10648444a6d9dea51e7
Robert Bell <rfb0010@auburn.edu>
2023-02-21 14:47:39 -0600

    Packages Portion of Assignment 1

---
+++ assignment 1 packages.R
@@ -0,0 +1,20 @@
+#5.	10 pts. Install the following packages, and show me, using code, that they are installed, and you can use them. 
+
+#For these packages i will be using the tips file
+
+#ggplot2
+install.packages("ggplot2")
+library(ggplot2)
+
+
+#dplyr
+install.packages("dplyr")
+
+#purr
+install.packages("purr")
+
+#lme4
+install.packages("lme4")
+
+
+s#emmeans
